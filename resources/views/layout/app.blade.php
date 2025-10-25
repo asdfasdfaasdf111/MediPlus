d6c3f07a46ac242ab9142305a89ea380d80b11f0
tiaraik19 <tiaraintan1904@gmail.com>
2025-10-25 14:51:34 +0700

    Kelola Jenis Pemeriksaan dan Kelola Modalitas

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,6 +6,8 @@
   <title>@yield('title', 'Mediplus')</title>
   <link rel="stylesheet" href="{{ asset('bootstrap5/css/bootstrap.min.css') }}">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+  @vite(['resources/js/inline-edit.js'])
+
 </head>
 <body class="bg-white text-dark">
 
